2bbb4bc0ecfc825be982e13cc41de37999e10d55
zolifyfr <jayjaynix1@gmail.com>
2026-06-28 13:26:01 -0400

    Add files via upload

---
+++ Lesson2Activty2.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x= 7
+y= "Jayden"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
